ec8d3779ea87449d17b5b987b89e672c2b192984
kvamsi7 <vk84351@gmail.com>
2024-11-22 01:36:01 -0600

    add model training

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.kidney_cnn_classifier import logger
 from src.kidney_cnn_classifier.pipeline.stage_01_data_ingestion import Data_ingestion_training_pipeline
 from src.kidney_cnn_classifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from src.kidney_cnn_classifier.pipeline.stage_03_model_training import ModelTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -23,6 +24,18 @@ try:
     prepare_base_model = PrepareBaseModelTrainingPipeline()
     prepare_base_model.main()
     logger.info(f">>>>>>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx============x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = 'Training'
+try:
+    logger.info(f"*****************")
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<<<")
+    obj = ModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<<<\n\nx=========x")
 except Exception as e:
     logger.exception(e)
     raise e
